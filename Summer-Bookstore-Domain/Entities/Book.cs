68df9171e2e6c65b1382f3417f33c2d7aba4ee13
kkhashayar <frankdezz@gmail.com>
2025-07-03 14:11:31 +0200

    Added simple book class with author navigation property

--- Summer-Bookstore-Domain/Entities/Book.cs
+++ Summer-Bookstore-Domain/Entities/Book.cs
@@ -1,11 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-namespace Summer_Bookstore_Domain.Entities
+﻿
+
+namespace Summer_Bookstore_Domain.Entities;
+
+public class Book
 {
-    internal class Book
-    {
-    }
+    public int Id { get; set; }
+    public string Title  { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public DateTime PublishedDate { get; set; }
+
+    // Foreign Key  
+    public int AuthorId { get; set; }
+
+    // Navigation Property
+    public Author? Author { get; set; }
+
 }
+
