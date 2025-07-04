5dc6390d066750ceacae616cbbec114afd2ab807
kkhashayar <frankdezz@gmail.com>
2025-07-04 23:29:04 +0200

    Added migrations, update-database

--- Summer-Bookstore/Program.cs
+++ Summer-Bookstore/Program.cs
@@ -3,21 +3,17 @@ using Summer_Bookstore_Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-// Registering db context
+// Registering DbContext
 builder.Services.AddDbContext<BookstoreDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-
 builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -25,7 +21,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
