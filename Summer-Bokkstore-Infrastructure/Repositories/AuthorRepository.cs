efa803f6f0794c38ad0bae96e604f764e709c7cd
kkhashayar <frankdezz@gmail.com>
2025-07-05 13:14:57 +0200

    Added AuthorRepository with empty implementation

---
+++ Summer-Bokkstore-Infrastructure/Repositories/AuthorRepository.cs
@@ -0,0 +1,42 @@
+﻿
+using Summer_Bookstore_Domain.Entities;
+
+namespace Summer_Bookstore_Infrastructure.Repositories;
+
+public class AuthorRepository : IAuthorRepository
+{
+    public Task AddAsync(Author author)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Author>> GetAllAuthorsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Author> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Author> GetByNameAsync(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SaveChangesAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Update(Author author)
+    {
+        throw new NotImplementedException();
+    }
+}
