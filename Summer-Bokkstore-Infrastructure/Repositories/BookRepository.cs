66b08507f07c576feae9ee18cca6d4da75bf4c49
kkhashayar <frankdezz@gmail.com>
2025-07-05 08:48:41 +0200

    Added comment in book repository.

--- Summer-Bokkstore-Infrastructure/Repositories/BookRepository.cs
+++ Summer-Bokkstore-Infrastructure/Repositories/BookRepository.cs
@@ -1,11 +1,21 @@
 ﻿
 using Summer_Bokkstore_Infrastructure.Interfaces;
 using Summer_Bookstore_Domain.Entities;
+using Summer_Bookstore_Infrastructure.Data;
 
 namespace Summer_Bookstore_Infrastructure.Repositories;
 
 public class BookRepository : IBookRepository
 {
+
+    private readonly BookstoreDbContext _bookContext;
+    public BookRepository(BookstoreDbContext bookContext)
+    {
+        _bookContext = bookContext; 
+    }
+    // One thing to consider is whether or not we should include author information.
+    // We could add a flag like `bool includeAuthor` and use it to conditionally include the author.
+    // Leaving it out for the sake of simplicity for now.
 
     public Task<Book> GetById(int id)
     {
