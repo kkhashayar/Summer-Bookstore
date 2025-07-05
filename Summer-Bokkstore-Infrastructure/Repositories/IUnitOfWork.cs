6300cb3754f59ed6ba2b84609e25eff63ab0b446
kkhashayar <frankdezz@gmail.com>
2025-07-05 11:25:52 +0200

    Addded unit of work interface with Ibookrepository property

---
+++ Summer-Bokkstore-Infrastructure/Repositories/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿
+using Summer_Bokkstore_Infrastructure.Interfaces;
+using System.Net;
+
+namespace Summer_Bookstore_Infrastructure.Repositories;
+
+public interface IUnitOfWork
+{
+    IBookRepository Books { get; }  
+
+    Task<int> CompleteAsync(); // This method will save changes to the database 
+
+}
