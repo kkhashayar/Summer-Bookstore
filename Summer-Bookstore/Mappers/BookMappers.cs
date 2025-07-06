fc66bc284956f8e312775d6c7cd311072ad0a191
kkhashayar <frankdezz@gmail.com>
2025-07-06 11:54:40 +0200

    Added for member mapping Date to datetime and reverse

--- Summer-Bookstore/Mappers/BookMappers.cs
+++ Summer-Bookstore/Mappers/BookMappers.cs
@@ -8,6 +8,9 @@ public class BookMappers : Profile
 {
     public BookMappers()
     {
-        CreateMap<BookCreateDto, Book>().ReverseMap();  
+        CreateMap<BookCreateDto, Book>()
+            .ForMember(dest => dest.PublishedDate, opt => opt.MapFrom(src => src.PublishedDate.ToDateTime(TimeOnly.MinValue))) // Convert DateOnly to DateTime
+            .ReverseMap()
+            .ForMember(dest => dest.PublishedDate, opt => opt.MapFrom(src => DateOnly.FromDateTime(src.PublishedDate))); // Convert DateTime to DateOnly
     }
 }
