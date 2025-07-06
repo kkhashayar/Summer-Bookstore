03c731c3cdc872e62a55a9ed7ef8ec6c0ab7e99f
kkhashayar <frankdezz@gmail.com>
2025-07-06 08:31:53 +0200

    Added: BooksControllers in api under controllers directory. added two end-points: GetById and GetByTitle

---
+++ Summer-Bookstore/Controllers/BooksControllers.cs
@@ -0,0 +1,43 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Summer_Bookstore_Infrastructure.Repositories;
+
+namespace Summer_Bookstore.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BooksControllers : ControllerBase
+{
+    readonly IUnitOfWork _unitOfWork;
+    readonly ILogger<BooksControllers> _logger;
+    public BooksControllers(IUnitOfWork unitOfWork, ILogger<BooksControllers> logger)
+    {
+        _logger = logger;
+        _unitOfWork = unitOfWork;
+    }
+
+
+    [HttpGet("id")]
+    public async Task<IActionResult> GetBookById(int id)
+    {
+        var book = await _unitOfWork.BookRepository.GetByIdAsync(id);
+        if (book == null)
+        {
+            _logger.LogWarning($"Book with ID {id} not found at: {DateTime.Now}.");
+            return NotFound($"Book with ID {id} not found.");
+        }
+        return Ok(book);
+    }
+
+    [HttpGet("title")]
+    public async Task<IActionResult> GetBookByTitle(string title)
+    {
+        var book = await _unitOfWork.BookRepository.GetByTitleAsync(title);
+        if (book == null)
+        {
+            _logger.LogWarning($"Book with title '{title}' not found at: {DateTime.Now}.");
+            return NotFound($"Book with title '{title}' not found.");
+        }
+        return Ok(book);
+    }
+}
