efa5a2b052986d8d7df2c465df7321aa0347068e
kkhashayar <frankdezz@gmail.com>
2025-07-06 11:03:51 +0200

    Added: Attribute on publishedDate, to make system that store only Date, and not effect database on how storing data.

--- Summer-Bookstore-Domain/Entities/Book.cs
+++ Summer-Bookstore-Domain/Entities/Book.cs
@@ -1,10 +1,14 @@
-﻿namespace Summer_Bookstore_Domain.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Summer_Bookstore_Domain.Entities;
 
 public class Book
 {
     public int Id { get; set; }
     public string Title  { get; set; } = string.Empty;
     public string? Description { get; set; }
+
+    [DataType(DataType.Date)] // Has no effect on how it stored in Database
     public DateTime PublishedDate { get; set; }
 
     // Foreign Key  
