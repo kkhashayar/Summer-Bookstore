e0d42dfef3312ab7eee6cf6c230d940689836109
kkhashayar <frankdezz@gmail.com>
2025-07-08 14:03:33 +0200

    Added bookReadDto under DTOs

---
+++ Summer-Bookstore/DTOs/BookReadDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Summer_Bookstore.DTOs;
+
+public class BookReadDto
+{
+    public string Title { get; set; } = string.Empty;
+    public string? AuthorName { get; set; }
+    public string? Description { get; set; }
+    public DateOnly PublishedDate { get; set; }
+
+}
