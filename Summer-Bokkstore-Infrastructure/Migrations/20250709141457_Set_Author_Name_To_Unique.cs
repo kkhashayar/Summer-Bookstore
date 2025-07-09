874452b57ce6e00db11cd256316ef0aa9d531829
kkhashayar <frankdezz@gmail.com>
2025-07-09 16:15:37 +0200

    Added: Migration and update database to set author name to unique.

---
+++ Summer-Bokkstore-Infrastructure/Migrations/20250709141457_Set_Author_Name_To_Unique.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Summer_Bookstore_Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Set_Author_Name_To_Unique : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Books");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Authors",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Authors_Name",
+                table: "Authors",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Authors_Name",
+                table: "Authors");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Authors",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "");
+        }
+    }
+}
