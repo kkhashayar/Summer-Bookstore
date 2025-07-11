440d025b66d8d939b408e68d21a55a3edff0277f
kkhashayar <frankdezz@gmail.com>
2025-07-11 18:07:34 +0200

    Comment

--- Summer-Bookstore/DTOs/AuthorCreateDto.cs
+++ Summer-Bookstore/DTOs/AuthorCreateDto.cs
@@ -5,5 +5,9 @@ namespace Summer_Bookstore.DTOs;
 public class AuthorCreateDto
 {
     [Required(ErrorMessage = "Name is required")]
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
 }
+/*
+ * Used Data Annotations for validation, specifically the Required attribute to ensure that the Name 
+ * property is not null or empty.
+ */
