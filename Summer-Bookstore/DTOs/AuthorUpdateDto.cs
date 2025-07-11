a835eec41204ba3863b298e70e9171a8fd8bb3ee
kkhashayar <frankdezz@gmail.com>
2025-07-11 18:14:54 +0200

    Added: two more validations that checks if Id of book or Author during update is greater than 0

--- Summer-Bookstore/DTOs/AuthorUpdateDto.cs
+++ Summer-Bookstore/DTOs/AuthorUpdateDto.cs
@@ -4,7 +4,8 @@ namespace Summer_Bookstore.DTOs;
 
 public class AuthorUpdateDto : AuthorCreateDto
 {
-    [Required(ErrorMessage = "Id is required")] 
+    [Required(ErrorMessage = "Id is required")]
+    [Range(1, int.MaxValue, ErrorMessage = "Id must be greater than 0.")]
     public int Id { get; set; }
 
 }
--- Summer-Bookstore/DTOs/BookUpdateDto.cs
+++ Summer-Bookstore/DTOs/BookUpdateDto.cs
@@ -5,7 +5,9 @@ namespace Summer_Bookstore.DTOs;
 public class BookUpdateDto
 {
     [Required(ErrorMessage = "Id is required")]
+    [Range(1, int.MaxValue, ErrorMessage = "Id must be greater than 0.")]
     public int Id { get; set; }
+    [Required(ErrorMessage = "Title is required")]
     public string Title { get; set; } = string.Empty;
     public string? Description { get; set; }
     public DateOnly PublishedDate { get; set; }
