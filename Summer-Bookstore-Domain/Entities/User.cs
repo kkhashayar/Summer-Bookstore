4e89c3b13b462c393415c44ab21ee907e33bd739
kkhashayar <frankdezz@gmail.com>
2025-07-13 17:07:45 +0200

    Added. Starting security structure by adding user class with basic properties and Enum Roles

---
+++ Summer-Bookstore-Domain/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using Summer_Bookstore_Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Summer_Bookstore_Domain.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    [Required]
+    [StringLength(50, ErrorMessage = "Username cannot be longer than 100 characters.")]
+    public string Username { get; set; } = string.Empty;
+    [Required]
+    public byte[] PasswordHash { get; set; }
+
+    public Roles Role { get; set; }
+}
