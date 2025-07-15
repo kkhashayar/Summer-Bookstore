5d74454fbefb23c8d9c0e9632495c202aaa133e7
kkhashayar <frankdezz@gmail.com>
2025-07-15 14:18:58 +0200

    Manually edited the dbcontext to make passwordHash nullable.

---
+++ Summer-Bokkstore-Infrastructure/Migrations/20250715121540_Forced_Nullable_PasswordHash_In_User.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Summer_Bookstore_Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Forced_Nullable_PasswordHash_In_User : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "varbinary(max)",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0],
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(max)",
+                oldNullable: true);
+        }
+    }
+}
