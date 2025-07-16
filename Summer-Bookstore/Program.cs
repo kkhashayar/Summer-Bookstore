d13c752519ff762052270514bc4e9f96d169a0e1
kkhashayar <frankdezz@gmail.com>
2025-07-16 11:25:31 +0200

    Added: Registered JwSettings in DI container.

--- Summer-Bookstore/Program.cs
+++ Summer-Bookstore/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Summer_Bokkstore_Infrastructure.Interfaces;
 using Summer_Bookstore.Application.Services;
+using Summer_Bookstore.Application.Settings;
 using Summer_Bookstore.Mappers;
 using Summer_Bookstore_Domain.Entities;
 using Summer_Bookstore_Infrastructure;
@@ -22,15 +23,18 @@ builder.Services.AddScoped<IBookRepository, BookRepository>();
 builder.Services.AddScoped<IUserService, UserService>();
 
 // Register automapper.
-
 // Fix for CS1503: Argument 2: cannot convert from 'System.Reflection.Assembly' Code pilot help :D
 // to 'System.Action<AutoMapper.IMapperConfigurationExpression>'
 builder.Services.AddAutoMapper(config => { config.AddMaps(typeof(BookMappers).Assembly); });
 builder.Services.AddAutoMapper(config => { config.AddMaps(typeof(AuthorMappers).Assembly); });
-builder.Services.AddAutoMapper(config => { config.AddMaps(typeof(UserRegisterMappers).Assembly); });    
+builder.Services.AddAutoMapper(config => { config.AddMaps(typeof(UserRegisterMappers).Assembly); });
 
-// Controllers asnd swagger 
 
+// Registering JwSettings 
+// This tells .NET to look inside appsettings.json for a section called "JwtSettings" and bind it to JwSettings class.
+builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwSettings"));
+
+// Controllers asnd swagger
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
